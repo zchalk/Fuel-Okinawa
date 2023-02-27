fa56564cdd7e41543faa1ddf7a0a71b29262d79a
zchalk <zoechalk1@gmail.com>
2023-02-27 20:12:47 +0900

    added responsiveness, japanese menu toggle, fixed connect page, added content to about

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+function japaneseToggle() {
+    var menu = document.getElementById("menuImg");
+    var drinks = document.getElementById("drinksImg");
+    console.log(menu.src);
+    console.log(drinks);
+    var button = document.getElementById("toggle");
+    console.log(button);
+        if (button.innerText == "日本語メニュー") {
+            menu.src = "assets/images/Menu-JAP.jpeg";
+            drinks.src = "assets/images/Drinks-JAP.jpeg";
+            button.innerText = "English Menu?";
+        }else if (button.innerText = "English Menu") {
+            menu.src = "assets/images/Menu-ENG.jpeg";
+            drinks.src = "assets/images/Drinks-ENG.jpeg";
+            button.innerText = "日本語メニュー";
+        }
+}
+
+// button.addEventListener("click", japaneseToggle);
